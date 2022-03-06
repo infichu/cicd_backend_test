d97ee8885bb970e6c13f01602486a7a44a2196ed
Qiaochu <qiaochu@scantist.com>
2022-03-06 19:51:35 +0800

    测试 忽略

---
+++ create_file.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+Author: Infi_chu
+"""
+
+
+def write_file():
+    with open('file.txt', 'w') as f:
+        f.write("This is test file!\n Success")
+
+
+if __name__ == '__main__':
+    write_file()
+
+
